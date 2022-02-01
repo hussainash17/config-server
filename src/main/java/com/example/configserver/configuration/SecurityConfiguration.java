07dac5458946ea02a5ea38e87f14f235e00fd101
kanababa317 <ashraf@dohatec.com.bd>
2022-02-01 16:52:22 +0600

    added

--- src/main/java/com/example/configserver/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/configserver/configuration/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.example.configserver.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
