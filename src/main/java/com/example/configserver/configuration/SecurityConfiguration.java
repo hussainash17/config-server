24c17d6424e34f8bb65a2e30037f5f553f838d84
kanababa317 <ashraf@dohatec.com.bd>
2022-02-02 13:43:51 +0600

    added new things

--- src/main/java/com/example/configserver/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/configserver/configuration/SecurityConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/decrypt").denyAll();
+        super.configure(http);
     }
 }
