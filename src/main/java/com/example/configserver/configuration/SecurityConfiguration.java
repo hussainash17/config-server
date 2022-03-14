c4b788325b6f6da45d9e414c0c83950da6eb7dde
kanababa <ashraf@dohatec.com.bd>
2022-03-14 09:20:37 +0600

    changes

--- src/main/java/com/example/configserver/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/configserver/configuration/SecurityConfiguration.java
@@ -1,15 +1,15 @@
-package com.example.configserver.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/decrypt").denyAll();
-        super.configure(http);
-    }
-}
+//package com.example.configserver.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().antMatchers("/decrypt").denyAll();
+//        super.configure(http);
+//    }
+//}
